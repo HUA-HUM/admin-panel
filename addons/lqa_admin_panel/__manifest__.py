a0411fae5079c983381a031ba739dc8cab79c3cd
Arturo Gutierrez <agutierrez.gyo@gmail.com>
2026-07-13 16:47:35 -0300

    Mejorar vista de clientes Xubio

--- addons/lqa_admin_panel/__manifest__.py
+++ addons/lqa_admin_panel/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "LQA Admin Panel",
-    "version": "18.0.1.0.35",
+    "version": "18.0.1.0.36",
     "category": "Sales",
     "summary": "Panel interno para areas operativas de Lo Quiero Aca",
     "author": "Lo Quiero Aca",
