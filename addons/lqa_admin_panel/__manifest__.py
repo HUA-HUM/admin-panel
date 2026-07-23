6fe957191b42014d16ffbb19ac776ebd54550b4b
Arturo Gutierrez <agutierrez.gyo@gmail.com>
2026-07-23 10:08:20 -0300

    Feature

--- addons/lqa_admin_panel/__manifest__.py
+++ addons/lqa_admin_panel/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "LQA Admin Panel",
-    "version": "18.0.1.0.48",
+    "version": "18.0.1.0.49",
     "category": "Sales",
     "summary": "Panel interno para areas operativas de Lo Quiero Aca",
     "author": "Lo Quiero Aca",
